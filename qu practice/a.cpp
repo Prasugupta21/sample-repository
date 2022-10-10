9bb968389c1bd6cef0b12a9b2c339e25714729df
Hari <prasugupta000000216@gmail.com>
2022-10-09 17:43:31 -0700

    added

---
+++ qu practice/a.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+int main()
+{
+   char a ='1';
+   cout<<a<<endl;
+   string s="abc";
+   auto p=s.find('a');
+   cout<<p<<endl;
+   return 0;
+}
